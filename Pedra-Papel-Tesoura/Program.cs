51945210d645f8f4a824da43547775ae53597e12
arthrs7 <juarhotmail295@gmail.com>
2024-10-01 13:47:53 -0300

    atualizando code

---
+++ Pedra-Papel-Tesoura/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Vamos jogar Papel, Tesoura!");
+
+        Console.WriteLine("Digite 1 para Papel, 2 para Tesoura, 3 para Pedra:");
+        int jogadorEscolha = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+        int computadorEscolha = random.Next(1, 4);
+
+        Console.WriteLine($"Você escolheu: {jogadorEscolha}");
+        Console.WriteLine($"O computador escolheu: {computadorEscolha}");
+
+        if (jogadorEscolha == computadorEscolha)
+        {
+            Console.WriteLine("Empate!");
+        }
+        else if ((jogadorEscolha == 1 && computadorEscolha == 3) ||
+                 (jogadorEscolha == 2 && computadorEscolha == 1) ||
+                 (jogadorEscolha == 3 && computadorEscolha == 2))
+        {
+            Console.WriteLine("Você ganhou!");
+        }
+        else
+        {
+            Console.WriteLine("Você perdeu!");
+        }
+    }
+}
